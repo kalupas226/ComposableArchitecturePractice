395a4605ab0fc8a2aa16ca9d12cc26368e5e3820
Kenta Aikawa <kenta.aikawa@sansan.com>
2020-07-01 02:32:39 +0900

    Todo list complete

--- ComposableArchitecturePractice/ContentView.swift
+++ ComposableArchitecturePractice/ContentView.swift
@@ -6,16 +6,76 @@
 //  Copyright © 2020 Aikawa Kenta. All rights reserved.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
+struct Todo: Equatable, Identifiable {
+    let id: UUID
+    var description = ""
+    var isComplete = false
+}
+
+struct AppState: Equatable {
+    var todos: [Todo] = []
+}
+
+enum AppAction {
+    
+}
+
+struct AppEnvironment {
+
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+        
+    }
+}
+
 struct ContentView: View {
+    let store: Store<AppState, AppAction>
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            WithViewStore(self.store) { viewStore in
+                List {
+                    ForEach(viewStore.todos) { todo in
+                        Text(todo.description)
+                    }
+                }
+            }
+        .navigationBarTitle("Todos")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store(
+                initialState: AppState(
+                    todos: [
+                        Todo(
+                            id: UUID(),
+                            description: "Milk",
+                            isComplete: false
+                        ),
+                        Todo(
+                            id: UUID(),
+                            description: "Eggs",
+                            isComplete: false
+                        ),
+                        Todo(
+                            id: UUID(),
+                            description: "Hand Soup",
+                            isComplete: false
+                        )
+                    ]
+                ),
+                reducer: appReducer,
+                environment: AppEnvironment()
+            )
+        )
     }
 }
