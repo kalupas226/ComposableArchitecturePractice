5ef04623e342e42d0fc3d06c1ffe4804f621994e
Kenta Aikawa <kenta.aikawa@sansan.com>
2020-07-03 21:51:36 +0900

    testTodoSorting_Cancellationのテストを追加

--- ComposableArchitecturePracticeTests/ComposableArchitecturePracticeTests.swift
+++ ComposableArchitecturePracticeTests/ComposableArchitecturePracticeTests.swift
@@ -57,19 +57,53 @@ class ComposableArchitecturePracticeTests: XCTestCase {
         
         store.assert(
             .send(.todo(index: 0, action: .checkboxTapped)) {
-                $0.todos = [
-                    Todo(
-                      id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
-                      description: "Eggs",
-                      isComplete: false
-                    ),
-                    Todo(
-                      id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
-                      description: "Milk",
-                      isComplete: true
-                    )
-                ]
+                $0.todos[0].isComplete = true
+            },
+            .do {
+                _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 1)
+            },
+            .receive(.todoDelayCompleted) {
+                $0.todos.swapAt(0, 1)
             }
+        )
+    }
+    
+    func testTodoSorting_Cancellation() {
+        let todos = [
+            Todo(
+                id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                description: "Milk",
+                isComplete: false
+            ),
+            Todo(
+                id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!,
+                description: "Eggs",
+                isComplete: false
+            )
+        ]
+        
+        let store = TestStore(
+            initialState: AppState(todos: todos),
+            reducer: appReducer,
+            environment: AppEnvironment(
+                uuid: { fatalError("unimplemented") }
+            )
+        )
+        
+        store.assert(
+            .send(.todo(index: 0, action: .checkboxTapped)) {
+                $0.todos[0].isComplete = true
+            },
+            .do {
+                _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 0.5)
+            },
+            .send(.todo(index: 0, action: .checkboxTapped)) {
+                $0.todos[0].isComplete = false
+                },
+            .do {
+                _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 1)
+                },
+            .receive(.todoDelayCompleted)
         )
     }
 }
